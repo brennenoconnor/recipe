cb606540c93545352f09016a7c1d1adfabb5e135
Brennen O'Connor <brennenoconnor@Brennens-MacBook-Pro.local>
2020-10-20 19:41:14 -0400

    added API key

---
+++ recipe-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = "e13de3c4";
+  const APP_KEY = "d4fa2a9b16cd003c26a07c922ee46a8b";
+
+  const// this is where we left off
+
+  return(
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+};
+export default App;
